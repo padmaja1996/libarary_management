98a9613085fb215fe1bd756bead3359ad0d04c3f
padmaja <gadyanavarpadmaja@gmail.com>
2022-02-11 12:01:35 +0530

    This is a new commit for what I originally planned to be amended

---
+++ src/Front/About.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import  '../Front/Css/Footer.css'
+import Clock from 'react-live-clock';
+
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+
+        <h1>Scenario Of Project</h1>
+                <div  class="row">
+                    <div className="col-sm-1"></div>
+					<div className="col-sm-10 csshobe">
+				   <p > <b className="fontsize">
+                   The Library Management System 
+                   </b> </p>
+				 </div>
+				 </div>
+
+
+                
+            </div>
+        )
+    }
+}
